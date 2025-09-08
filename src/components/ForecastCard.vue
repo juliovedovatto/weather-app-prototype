181a4b15c35f77d328969f8a50748812c0ab6a7f
Julio Vedovatto <juliovedovatto@gmail.com>
2025-09-08 01:41:25 -0300

    refactor(ui): add ForecastCard component and use in ForecastCardsRow

---
+++ src/components/ForecastCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="flex w-full flex-shrink-0 flex-row items-center rounded-card-mobile bg-wx-sky-50 px-6 py-5-5 text-left shadow-sm ring-1 ring-black/5 sm:w-32 sm:flex-col sm:items-center sm:rounded-card sm:text-center sm:md:w-36"
+  >
+    <img src="/images/snow.svg" alt="Snowing" class="h-10 w-10" />
+    <div class="ml-4 flex flex-col sm:ml-0">
+      <div class="font-semibold sm:mt-4">Today</div>
+      <div class="text-xs text-wx-gray-400">Frozen</div>
+    </div>
+    <div class="ml-auto text-xl font-semibold sm:mt-4 sm:ml-0">2 °C</div>
+  </div>
+</template>
--- src/components/ForecastCardsRow.vue
+++ src/components/ForecastCardsRow.vue
@@ -1,19 +1,13 @@
 <script setup lang="ts">
+import ForecastCard from './ForecastCard.vue';
+
 // TODO: accept array of forecast days via props.
 </script>
 
 <template>
   <div class="flex flex-col gap-4 overflow-visible pb-1 sm:flex-row sm:gap-6">
-    <div
-      class="flex w-full flex-shrink-0 flex-row items-center rounded-card-mobile bg-wx-sky-50 px-6 py-5-5 text-left shadow-sm ring-1 ring-black/5 sm:w-32 sm:flex-col sm:items-center sm:rounded-card sm:text-center sm:md:w-36"
-    >
-      <img src="/images/snow.svg" alt="Snowing" class="h-10 w-10" />
-      <div class="ml-4 flex flex-col sm:ml-0">
-        <div class="font-semibold sm:mt-4">Today</div>
-        <div class="text-xs text-wx-gray-400">Frozen</div>
-      </div>
-      <div class="ml-auto text-xl font-semibold sm:mt-4 sm:ml-0">2 °C</div>
-    </div>
+    <ForecastCard />
+
     <div
       class="flex w-full flex-shrink-0 flex-row items-center rounded-card-mobile bg-wx-sky-100 px-6 py-5-5 text-left shadow-sm ring-1 ring-black/5 sm:w-32 sm:flex-col sm:items-center sm:rounded-card sm:text-center sm:md:w-36"
     >
