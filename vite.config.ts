13c9da52e1a3e87960c2fc005539c676f6c3fc4e
Julio Vedovatto <juliovedovatto@gmail.com>
2025-09-09 14:32:39 -0300

    feat(test): testr environment setup

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
+    testTimeout: 10000,
+    restoreMocks: true, // clears history for all spies and mocks, and restores mocks to their original mocked implementation after each test
+    unstubEnvs: true,
+    unstubGlobals: true,
+  },
 });
