e0449058a6b953c6867bc28b1920843c8ef10df0
Julio Vedovatto <juliovedovatto@gmail.com>
2025-09-09 18:10:56 -0300

    fix(test): update vitest to the latest version

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,31 @@ import { fileURLToPath, URL } from 'node:url';
 
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // eslint-disable-next-line no-console
+  console.log(`Vite mode: ${mode}`);
+
+  return {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
+    plugins: [vue(), tailwindcss()],
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html'],
+      },
+      testTimeout: 10000,
+      restoreMocks: true,
+      unstubEnvs: true,
+      unstubGlobals: true,
     },
-    testTimeout: 10000,
-    restoreMocks: true, // clears history for all spies and mocks, and restores mocks to their original mocked implementation after each test
-    unstubEnvs: true,
-    unstubGlobals: true,
-  },
+  };
 });
