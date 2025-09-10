e1ad54b9aa7a87c73a7f905d2fcacb514e40a795
Julio Vedovatto <juliovedovatto@gmail.com>
2025-09-09 21:28:25 -0300

    feat(test): improve global setup for tests

--- tests/setup-env.ts
+++ tests/setup-env.ts
@@ -1,7 +1,32 @@
-import { vi } from 'vitest';
+import { flushPromises } from '@vue/test-utils';
+import { beforeEach, afterEach, vi } from 'vitest';
 
 import type { Ref } from 'vue';
+
+import queryClient from '@/queries/client';
+
+function clearCookies() {
+  document.cookie.split(';').forEach((cookie) => {
+    document.cookie = cookie.replace(/^ +/, '').replace(/=.*/, `=;expires=${new Date(0).toUTCString()}`);
+  });
+}
+
+// Mocks
 
 vi.mock('@vueuse/core', () => ({
   useDebounce: <T>(r: Ref<T>) => r,
 }));
+
+// Lifecycle hooks
+
+beforeEach(() => {
+  queryClient.clear();
+});
+
+afterEach(async () => {
+  // Prevent instances of UnmockedRequestError between tests
+  await flushPromises();
+
+  window.localStorage.clear();
+  clearCookies();
+});
